8ea5aac00f03c785018c2c02a142d585f3e1049e
nhat-anh <japananh@gmail.com>
2019-04-02 22:09:58 +0700

    Add README and 5 sorting algorithms

--- sorting/index.js
+++ sorting/index.js
@@ -6,8 +6,8 @@ const quickSort = require('./quick-sort');
 
 let arr = [10, 34, 53, -33, 100, 13, 90.45, -3000, 4.89];
 
-bubbleSort.bubbleSort(arr);
-console.log(selectionSort.selectionSort(bubbleSort.bubbleSort(arr))); // this function has an issue
-insertionSort.insertionSort(arr);
-mergeSort.mergeSort(arr);
-quickSort.quickSort(arr);
+// console.log(bubbleSort.bubbleSort(arr));
+// console.log(selectionSort.selectionSort(arr));
+// console.log(insertionSort.insertionSort(arr));
+// console.log(mergeSort.mergeSort(arr));
+// console.log(quickSort.quickSort(arr));
--- sorting/selection-sort.js
+++ sorting/selection-sort.js
@@ -1,7 +1,7 @@
 const swap = require('./swap.js');
 
 function selectionSort(arr) {
-  let min = 0;
+  let min = 1;
   
   for (let i = 0, len = arr.length; i < len - 1; i++) {
     for (let j = i + 1; j < len; j++) {
@@ -13,6 +13,8 @@ function selectionSort(arr) {
     if (arr[i] > arr[min]) {
       swap.swap(arr, i, min);
     }
+    
+    ++min;
   }
 
   return arr;
